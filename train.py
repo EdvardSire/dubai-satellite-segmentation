220f193608db6257cde5a9ff7844167654104cee
Edvard Sire <mail@edvardsire.com>
2025-03-09 02:39:09 +0100

    imports

--- train.py
+++ train.py
@@ -1,17 +1,11 @@
 import os
+from pathlib import Path
 import torch
+from torch.optim import lr_scheduler
+import torch.optim as optim
 import torchvision
 from torchvision import transforms
-import torch.optim as optim
 import segmentation_models_pytorch as smp
-import albumentations as A
-from pathlib import Path
-import torch
-
-from pathlib import Path
-
-from torch.optim import lr_scheduler
-
 from dataset import *
 
 torch.manual_seed(42)
@@ -30,14 +24,11 @@ def save_model(model: torch.nn.Module, target_dir: str, epoch: int):
 
 
 if __name__ == '__main__':
-    # Calculate means and stds of the trainset and normalize
-
     dataset_split = Path('dataset_split')
-    train_data = torchvision.datasets.ImageFolder(root = dataset_split/'train', transform = transforms.ToTensor())
+    train_data = torchvision.datasets.ImageFolder(root = str(dataset_split / 'train'), transform = transforms.ToTensor())
 
     means = torch.zeros(3)
     stds = torch.zeros(3)
-
     for img, label in train_data:
         means += torch.mean(img, dim = (1,2))
         stds += torch.std(img, dim = (1,2))
@@ -48,22 +39,21 @@ if __name__ == '__main__':
     print(f'Calculated means: {means}')
     print(f'Calculated stds: {stds}')
 
-    import albumentations as A
-
-    data_augmentation = {
-        'train': A.Compose([
-            A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=30, p=0.5),
-            A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.5),
-            A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.5),
-            A.Normalize(mean=means, std=stds),
-        ]),
-        'val': A.Compose([
-            A.Normalize(mean=means, std=stds),
-        ]),
-        'test': A.Compose([
-            A.Normalize(mean=means, std=stds),
-        ]),
-    }
+    # import albumentations as A
+    # data_augmentation = {
+    #     'train': A.Compose([
+    #         A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2, rotate_limit=30, p=0.5),
+    #         A.RGBShift(r_shift_limit=25, g_shift_limit=25, b_shift_limit=25, p=0.5),
+    #         A.RandomBrightnessContrast(brightness_limit=0.3, contrast_limit=0.3, p=0.5),
+    #         A.Normalize(mean=means, std=stds),
+    #     ]),
+    #     'val': A.Compose([
+    #         A.Normalize(mean=means, std=stds),
+    #     ]),
+    #     'test': A.Compose([
+    #         A.Normalize(mean=means, std=stds),
+    #     ]),
+    # }
     
 
 
